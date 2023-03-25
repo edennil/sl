103f5e318c08c2e722ef963f9c1ee8e6af757968
Vicent <vicent.altur@gmail.com>
2023-03-25 19:38:26 +0100

    Delete and add a wrong file

---
+++ test/tests/test_environment_test.cpp
@@ -0,0 +1,25 @@
+
+#include "test_env.hpp"
+#include "framework/helper.hpp"
+
+SL_TEST_GROUP(environment_test)
+
+SL_ADD_TEST_CASE(operations)
+{
+    double a = 2.;
+    double b = 2.;
+    
+    SL_TEST(a + b == 4.);
+    SL_TEST(a - b == 0.);
+    SL_TEST(a * b == 4.);
+    SL_TEST(a / b == 1.);
+
+    SL_TEST(a + b != 3.);
+    SL_TEST(a + b > 3.);
+    SL_TEST(a + b >= 4.);
+    SL_TEST(a + b < 5.);
+    SL_TEST(a + b <= 5.);
+}
+
+
+SL_END_TEST_GROUP

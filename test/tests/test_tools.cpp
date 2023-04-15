0ed78961487df2e547b1cca8f2d0d3d681447396
Vicent Altur Brines <vicent.altur@gmail.com>
2023-04-15 16:23:44 +0100

    Change the name to zip, and add some tests

--- test/tests/test_tools.cpp
+++ test/tests/test_tools.cpp
@@ -1,35 +1,65 @@
 #include "test_env.hpp"
 #include "framework/helper.hpp"
 
-#include <sl/tools/multi_container.hpp>
+#include <sl/tools/zip.hpp>
 
 void f(const std::vector<double>& l, const std::vector<double>& r)
 {
-    auto m = sl::tools::make_multi(l, r);
-
-    for (auto v : m)
+    for (auto v : sl::tools::zip(l, r))
     {
-        //std::get<0>(v) = 5;
         SL_TEST(std::get<0>(v) == std::get<1>(v));
     }
 }
 
 SL_TEST_GROUP(tools_test)
 
-SL_ADD_TEST_CASE(test_multi_container_vector)
+SL_ADD_TEST_CASE(test_zip_vector)
 {
     std::vector<double> a{1.,2.,3.};
-    std::vector<double> b{1.,2.2,3.};
+    std::vector<double> b{1.,2.,3.};
+    std::vector<double> c{ 1.,2.,3. };
+    std::vector<double> d{ 1.,2.,3. };
+    std::vector<double> e{ 1.,2.,3. };
 
-    //auto m = sl::tools::make_multi(a, b);
-
-    //for (auto v : m)
-    //{
-    //    std::get<0>(v) = 5;
-    //    //SL_TEST(std::get<0>(v) == std::get<1>(v));
-    //}
+    for (auto v : sl::tools::zip(a, b, c, d, e))
+    {
+        SL_TEST(std::get<0>(v) == std::get<4>(v));
+    }
 
     f(a, b);
 }
+
+SL_ADD_TEST_CASE(test_zip_map)
+{
+
+    std::map<std::string, int> a = { {"a", 1}, {"b", 2}};
+    std::map<std::string, int> b = { {"a", 1}, {"b", 2} };
+    std::unordered_map<std::string, int> c = { {"a", 1}, {"b", 2} };
+    std::unordered_map<std::string, int> d = { {"a", 1}, {"b", 2} };
+
+    for (auto v : sl::tools::zip(a, b, c, d))
+    {
+        SL_TEST(std::get<0>(v).second == std::get<1>(v).second);
+        SL_TEST(std::get<2>(v).second == std::get<3>(v).second);
+    }
+
+}
+
+SL_ADD_TEST_CASE(test_zip_vector_map)
+{
+
+    std::vector<double> a{ 1.,2.,3. };
+    std::vector<double> b{ 1.,2.,3. };
+    std::unordered_map<std::string, int> c = { {"a", 1}, {"b", 2} };
+    std::unordered_map<std::string, int> d = { {"a", 1}, {"b", 2} };
+
+    for (auto v : sl::tools::zip(a, b, c, d))
+    {
+        SL_TEST(std::get<0>(v) == std::get<1>(v));
+        SL_TEST(std::get<2>(v).second == std::get<3>(v).second);
+    }
+
+}
+
 
 SL_END_TEST_GROUP
